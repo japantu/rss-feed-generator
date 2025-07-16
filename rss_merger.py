0bd6e274092e7de142f85ad9dcb8b2f6b4524eaa
japantu <japantu0711@gmail.com>
2025-07-17 00:22:30 +0900

    Delete rss_merger.py

--- rss_merger.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Response, send_file, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "HEAD"])
-def serve_rss():
-    if request.method == "HEAD":
-        return Response("OK", status=200)
-    return send_file("rss_output.xml", mimetype="text/xml")
